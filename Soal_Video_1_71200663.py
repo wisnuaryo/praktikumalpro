988baa5ccb0bdde9a1ad107db9f1ceb136e43fac
wisnuaryo <79086382+wisnuaryo@users.noreply.github.com>
2021-02-16 14:11:52 +0900

    Add files via upload

---
+++ Soal_Video_1_71200663.py
@@ -0,0 +1,15 @@
+#71200663
+#Wisnu Aryo Jatmiko
+
+print("Kalkulator Menghitung Molaritas")
+
+#Larutan A
+gr = float(input("Masukan gram: "))
+mr = float(input("Masukan nilai MR: "))
+v = float(input("Masukan volume air(mililiter): "))
+
+rms = (gr/mr) * (1000/v)
+
+print("Nilai Molaritas adalah", rms)
+
+
